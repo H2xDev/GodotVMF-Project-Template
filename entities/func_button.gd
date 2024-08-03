1d7c8b7616b0f5d486abc713a9700c13dab25de7
H2xDev <h2xdeveloper@gmail.com>
2024-08-03 17:29:57 +0400

    Updatge func_button

--- entities/func_button.gd
+++ entities/func_button.gd
@@ -9,22 +9,31 @@ const FLAG_USE_ACTIVATES = 1024;
 const FLAG_SPARKS = 4096;
 
 var wait_time = 0.0;
+var click_sound = null;
+var lock_sound = null;
+
+func _entity_ready():
+	click_sound = entity.get("click_sound", "");
+	lock_sound = entity.get("lock_sound", "");
+
+	if click_sound: SoundManager.precache_sound(click_sound);
+	if lock_sound: SoundManager.precache_sound(lock_sound);
 
 func _interact(_player: Player):
 	if not has_flag(FLAG_USE_ACTIVATES):
 		return;
 
 	if is_locked:
+		SoundManager.play_sound(global_transform.origin, lock_sound);
 		trigger_output("OnUseLocked");
 		return;
 
 	if not has_flag(FLAG_DONT_MOVE):
 		if wait_time > 0.0: return;
 		wait_time = float(entity.wait);
-
-	if not has_flag(FLAG_DONT_MOVE):
 		Open(null);
 
+	SoundManager.play_sound(global_transform.origin, click_sound);
 	trigger_output("OnPressed");
 
 func _process(delta: float):
