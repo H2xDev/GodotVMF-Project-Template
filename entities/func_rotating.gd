42cc2c54619827d1a3df2cc93067e7b1da02989e
H2xDev <h2xdeveloper@gmail.com>
2024-08-06 01:34:11 +0400

    Fix func_rotating

--- entities/func_rotating.gd
+++ entities/func_rotating.gd
@@ -8,47 +8,51 @@ const FLAG_REVERSE_DIRECTION: int = 2;
 const FLAG_X_AXIS: int = 4;
 const FLAG_Y_AXIS: int = 8;
 
-var rotTween = null;
+var current_tween = null;
 
-# Called when the node enters the scene tree for the first time.
-func _process_physics(dt):
+func _apply_entity(e):
+	super._apply_entity(e);
+
+	$body/mesh.set_mesh(get_mesh());
+	$body/mesh.cast_shadow = entity.disableshadows == 0;
+	$body/collision.shape = get_entity_shape();
+
+func _physics_process(dt):
 	if Engine.is_editor_hint() && not preview:
 		return;
 
-	if not enabled:
-		return;
+	if not enabled: return;
 
 	var speed = entity.maxspeed * dt;
 
-	if typeof(entity.spawnflags) != TYPE_INT:
-		entity.spawnflags = int(entity.spawnflags);
-
-	if entity.spawnflags & FLAG_REVERSE_DIRECTION:
+	if has_flag(FLAG_REVERSE_DIRECTION):
 		speed *= -1;
 
-	if entity.spawnflags & FLAG_X_AXIS:
+	if has_flag(FLAG_X_AXIS):
 		rotation_degrees.x += speed;
-	elif entity.spawnflags & FLAG_Y_AXIS:
+	elif has_flag(FLAG_Y_AXIS):
 		rotation_degrees.z += speed;
 	else:
 		rotation_degrees.y += speed;
 
 func _entity_ready():
-	if Engine.is_editor_hint():
-		return;
-
-	if typeof(entity.spawnflags) != TYPE_INT:
-		entity.spawnflags = int(entity.spawnflags);
-
 	enabled = has_flag(FLAG_START_ON);
 
+# INPUTS
+
+func Start(_param = null):
+	enabled = true;
+
+func Stop(_param = null):
+	enabled = false;
+
 func RotateBy(deg):
-	if rotTween:
+	if current_tween:
 		return;
 
 	deg = float(deg);
 
-	rotTween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN);
+	current_tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN);
 	var target = Vector3.ZERO;
 
 	if has_flag(FLAG_X_AXIS):
@@ -58,15 +62,10 @@ func RotateBy(deg):
 	else:
 		target.y = deg;
 	
-	var endRot = rotation_degrees + target;
+	var end_rot = rotation_degrees + target;
 
-	rotTween.tween_property(self, "rotation_degrees", endRot, 2.0);
-	rotTween.play();
-	rotTween.finished.connect(func(): rotTween = null);
+	current_tween.tween_property(self, "rotation_degrees", end_rot, 2.0);
+	current_tween.play();
 
-func _apply_entity(e):
-	super._apply_entity(e);
-
-	$body/mesh.set_mesh(get_mesh());
-	$body/mesh.cast_shadow = entity.disableshadows == 0;
-	$body/collision.shape = get_entity_shape();
+	await current_tween.finished;
+	current_tween = null
