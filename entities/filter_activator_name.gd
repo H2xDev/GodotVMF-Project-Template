b69fed7f80dc4a6773c53276e96e3b61ce63c525
H2xDev <h2xdeveloper@gmail.com>
2024-08-07 23:12:52 +0400

    Fix filters

--- entities/filter_activator_name.gd
+++ entities/filter_activator_name.gd
@@ -7,8 +7,7 @@ func is_passed(node: Node3D) -> bool:
 	var target_entity = get_entity(node);
 
 	if not target_entity: return false;
-	
-	if is_inverted:
-		return target_entity.name == entity.get("targetname", "");
-	else:
-		return target_entity.name != entity.get("targetname", "");
+
+	var is_passed = target_entity.entity.get("targetname", "") == entity.get("filtername", "");
+
+	return is_passed if not is_inverted else not is_passed;
--- entities/trigger_multiple.gd
+++ entities/trigger_multiple.gd
@@ -4,11 +4,7 @@ extends ValveIONode
 
 const FLAG_CLIENTS = 1;
 
-var has_filter:
-	get: return entity.get("filtername", "") != "";
-
 func get_filter_entity() -> filter_entity:
-	if not has_filter: return null;
 	var target_entity = get_target(entity.get("filtername", ""));
 
 	if not target_entity: return null;
@@ -18,9 +14,9 @@ func get_filter_entity() -> filter_entity:
 func _entity_ready():
 	$area.body_entered.connect(func(body):
 		var is_client_passed = has_flag(FLAG_CLIENTS) and ValveIONode.aliases["!player"] == body;
-		var is_filter_passed = has_filter and get_filter_entity().is_passed(body);
 
-		prints("has_filter", has_filter);
+		var filter = get_filter_entity();
+		var is_filter_passed = filter.is_passed(body) if filter else false;
 
 		if is_client_passed or is_filter_passed:
 			trigger_output("OnTrigger");
@@ -29,7 +25,8 @@ func _entity_ready():
 
 	$area.body_exited.connect(func(body):
 		var is_client_passed = has_flag(FLAG_CLIENTS) and ValveIONode.aliases["!player"] == body;
-		var is_filter_passed = has_filter and get_filter_entity().is_passed(body);
+		var filter = get_filter_entity();
+		var is_filter_passed = filter.is_passed(body) if filter else false;
 
 		if is_client_passed or is_filter_passed: 
 			trigger_output("OnEndTouch");
