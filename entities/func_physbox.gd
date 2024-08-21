3f524981d95f2409eea1099963f3ac9c67b29528
H2xDev <h2xdeveloper@gmail.com>
2024-08-21 23:41:32 +0400

    Add Jolt Physics

--- entities/func_physbox.gd
+++ entities/func_physbox.gd
@@ -4,6 +4,11 @@ extends ValveIONode
 var FLAG_IGNORE_PICKUP = 8192;
 var FLAG_MOTION_DISABLED = 32768;
 
+var body = null;
+
+func _entity_ready():
+	reparent(get_tree().current_scene);
+
 func EnableMotion(_param):
 	$body.freeze = false;
 
@@ -12,9 +17,9 @@ func DisableMotion(_param):
 
 func _apply_entity(e):
 	super._apply_entity(e);
-
+	
 	$body.freeze = has_flag(FLAG_MOTION_DISABLED);
 	$body/mesh.set_mesh(get_mesh());
-	$body/collision.shape = get_entity_shape();
+	$body/collision.shape = $body/mesh.mesh.create_convex_shape(true, true);
 
 	$body.mass = $body/mesh.get_aabb().size.x * $body/mesh.get_aabb().size.y * $body/mesh.get_aabb().size.z * 0.01;
