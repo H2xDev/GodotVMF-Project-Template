6992cf6352b1e331176649ac5711010fddaa2659
H2xDev <h2xdeveloper@gmail.com>
2024-11-14 19:08:09 +0400

    Update entities

--- entities/trigger_once.gd
+++ entities/trigger_once.gd
@@ -3,8 +3,9 @@ class_name trigger_once
 extends ValveIONode
 
 func _entity_ready():
+	# NOTE: Call deffered is used to prevent unexpected trigger after player spawn
 	$area.body_entered.connect.call_deferred(func(_node):
-		if ValveIONode.aliases["!player"] == _node: 
+		if "!player" in ValveIONode.aliases and ValveIONode.aliases["!player"] == _node: 
 			trigger_output("OnTrigger");
 			queue_free();
 	);
